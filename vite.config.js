7407c5318c26aa5ea6b226a07fe1fa0b51d39d90
WAHIDI1970 <mouad066989gmail.com>
2026-06-27 15:23:05 +0200

    Keep only docs folder

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Remplacez <nom-de-votre-depot> par le nom exact de votre dépôt GitHub (ex: 'nick')
-  base: '/100wahidi/NICK/', 
-})
