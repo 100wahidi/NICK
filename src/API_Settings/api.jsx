ee95f7952e7aecc00cbfc566d58b69cdbf3a09de
WAHIDI1970 <mouad066989gmail.com>
2026-06-28 21:25:59 +0200

    prod with .env in github env

--- src/API_Settings/api.jsx
+++ src/API_Settings/api.jsx
@@ -1,4 +1,4 @@
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = "https://alice-production-9ec8.up.railway.app";
 // ✅ Generic request
 export async function apiRequest(endpoint, method = "POST", data = null, token = null) {
   try {
